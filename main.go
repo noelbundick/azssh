d7ea40f7889ed9a4461ea3bcb22780715da1017c
Noel Bundick <noelbundick@gmail.com>
2019-01-17 15:05:44 -0800

    Create a cloud shell & connect to it

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 
+	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/gorilla/websocket"
+	"github.com/pkg/browser"
+)
+
+const (
+	redirectURL string = "https://vscode-redirect.azurewebsites.net/"
+	clientID    string = "aebc6443-996d-45c2-90f0-388ff96faa56" // VS Code Azure Account extension
 )
 
 func dial(url string) *websocket.Conn {
@@ -42,9 +54,69 @@ func pumpInput(c *websocket.Conn, r io.Reader, done chan interface{}) {
 	}
 }
 
+func sendRequest(token string, method string, url string, payload string) map[string]interface{} {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("url:", url)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println("body:", string(body))
+
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+	return result
+}
+
+func createConsole(token string) string {
+	data := `{"properties": { "osType": "linux" } }`
+	result := sendRequest(token, "PUT", "https://management.azure.com/providers/Microsoft.Portal/consoles/default?api-version=2017-08-01-preview", data)
+	properties := result["properties"].(map[string]interface{})
+	return properties["uri"].(string)
+}
+
+func createTerminal(token string, consoleURL string) string {
+	url := fmt.Sprintf("%s/terminals?cols=80&rows=30&shell=bash", consoleURL)
+	data := `{"tokens": []}`
+	result := sendRequest(token, "POST", url, data)
+	return result["socketUri"].(string)
+}
+
+func getToken() string {
+	config, err := adal.NewOAuthConfig("https://login.microsoftonline.com/", "common")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sender := &http.Client{}
+	code, err := adal.InitiateDeviceAuth(sender, *config, clientID, "https://management.core.windows.net/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("code:", *code.Message)
+	browser.OpenURL(*code.VerificationURL)
+
+	token, err := adal.WaitForUserCompletion(sender, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return token.OAuthToken()
+}
+
 func provision() string {
-	url := os.Args[1]
-	return url
+	token := getToken()
+	consoleURL := createConsole(token)
+	terminalURI := createTerminal(token, consoleURL)
+	return terminalURI
 }
 
 func connect(url string) {
@@ -65,6 +137,9 @@ func connect(url string) {
 }
 
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(ioutil.Discard)
+
 	url := provision()
 	connect(url)
 }
