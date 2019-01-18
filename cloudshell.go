ee8f90922e007b1ce39154ed8e5025648e9fd113
Noel Bundick <noelbundick@gmail.com>
2019-01-18 15:30:48 -0800

    world's worst token cache/refresh

--- cloudshell.go
+++ cloudshell.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/pkg/browser"
@@ -56,10 +59,58 @@ func createTerminal(token string, consoleURL string) string {
 	return result["socketUri"].(string)
 }
 
+func getTokenCachePath() string {
+	u, err := user.Current()
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("%s/.azssh/token.json", u.HomeDir)
+}
+
+func saveToken(token adal.Token, tokenPath string) error {
+	tokenPathDir := filepath.Dir(tokenPath)
+	err := os.MkdirAll(tokenPathDir, os.ModePerm)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return adal.SaveToken(tokenPath, 0600, token)
+}
+
 func getToken() string {
+	tokenPath := getTokenCachePath()
 	config, err := adal.NewOAuthConfig("https://login.microsoftonline.com/", "common")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	refreshCallback := func(token adal.Token) error {
+		return saveToken(token, tokenPath)
+	}
+
+	token, err := adal.LoadToken(tokenPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if token != nil {
+		if true || token.IsExpired() {
+			spt, err := adal.NewServicePrincipalTokenFromManualToken(*config, clientID, "https://management.core.windows.net/", *token, refreshCallback)
+			if err != nil {
+				log.Println("token refresh failure:", err)
+			}
+			err = spt.Refresh()
+			if err != nil {
+				log.Println("token refresh failure:", err)
+			}
+
+			spToken := spt.Token()
+			token = &spToken
+		}
+
+		return token.OAuthToken()
 	}
 
 	sender := &http.Client{}
@@ -70,11 +121,19 @@ func getToken() string {
 	fmt.Println(*code.Message)
 	browser.OpenURL(*code.VerificationURL)
 
-	token, err := adal.WaitForUserCompletion(sender, code)
+	token, err = adal.WaitForUserCompletion(sender, code)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return token.OAuthToken()
+
+	spt, err := adal.NewServicePrincipalTokenFromManualToken(*config, clientID, "https://management.core.windows.net/", *token, nil)
+
+	err = saveToken(spt.Token(), tokenPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return spt.OAuthToken()
 }
 
 func Provision() string {
